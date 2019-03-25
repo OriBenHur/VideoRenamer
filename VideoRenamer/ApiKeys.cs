cdeb155cbc266cbab025fc5f13fa13ed319a965b
OriBdenHur <oribenhur@gmail.com>
2019-03-25 10:57:35 +0200

    Added a dummy ApiKeys.cs file

---
+++ VideoRenamer/ApiKeys.cs
@@ -0,0 +1,15 @@
+namespace SubDownloader
+{
+    internal class ApiKeys
+    {
+
+        public string TvdBapikey { get; }
+        public string TmdBapikey { get; }
+
+        public ApiKeys()
+        {
+            TvdBapikey = "Replace_With_TVDB_API_KEY";
+            TmdBapikey = "Replace_With_TMDB_API_KEY";
+        }
+    }
+}
